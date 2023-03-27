46fe0fee5060894e00c2ce8d754089d3bb4d74a6
Jenny Hartzog <jenny@nomorebeets>
2023-03-27 12:16:02 -0700

    added event listener to selection.

--- js/script.js
+++ js/script.js
@@ -1,15 +1,19 @@
 const randomFolks = document.querySelector(".random-peeps");
 
-const getData = async function () {
-    const usersRequest = await fetch("https://randomuser.me/api?results=5")
+const selectUserNumber = document.querySelector("#users");
+
+const getData = async function (numUsers) {
+    const usersRequest = await fetch(
+        `https://randomuser.me/api?results=${numUsers}`
+    );
     const data = await usersRequest.json();
 
-    let userResults = data.results;
+    const userResults = data.results;
     displayUsers(userResults);
 };
 
 
-getData();
+getData(1)
 
 const displayUsers = function(userResults) {
     randomFolks.innerHtml = "";
@@ -22,11 +26,16 @@ const displayUsers = function(userResults) {
     
         userDiv.innerHTML = `
             <h3>${name}</h3>
-        <p>${country}</p>
-        <img src=${imageUrl} alt="User avatar"/>
+            <p>${country}</p>
+            <img src=${imageUrl} alt="User avatar"/>
         `;
-    randomFolks.append(userDiv);
+        randomFolks.append(userDiv);
     }
     
-}
+};
+
+selectUserNumber.addEventListener("change", function(e){
+    const numUsers = e.target.value;
+    getData(numUsers);
+});
 
